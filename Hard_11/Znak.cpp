128a21f1a1918c33d7414d199b54742add07c34e
SavkovAndrey <141626360+SavkovAndrey@users.noreply.github.com>
2023-11-10 16:18:29 +0300

    конструкторы

--- Hard_11/Znak.cpp
+++ Hard_11/Znak.cpp
@@ -6,20 +6,34 @@ using namespace std;
 
 Znak::Znak() 
 {
-
+	this->name = "¿Ì‰ÂÈ";
+	this->zodiac = "—ÚÂÎÂˆ";
+	bday[0] = 11;
+	bday[1] = 12;
+	bday[2] = 1986;
 }
 //-------------------------  ŒÕ—“–” “Œ– — œ¿–¿Ã≈“–¿Ã»
 
 Znak::Znak(string name, string zodiac, int bday[])
 {
-
+	this->name = name;
+	this->zodiac = zodiac;
+	for (int i = 0; i < 3; i++)
+	{
+		this->bday[i] = bday[i];
+	}
 }
 
 //-------------------------  ŒÕ—“–” “Œ–  Œœ»–Œ¬¿Õ»ﬂ
 
 Znak::Znak(Znak& other) 
 {
-
+	this->name = other.name;
+	this->zodiac = other.zodiac;
+	for (int i = 0; i < 3; i++)
+	{
+		this->bday[i] = other.bday[i];
+	}
 }
 
 //------------------------- ƒ≈—“–” “Œ–
